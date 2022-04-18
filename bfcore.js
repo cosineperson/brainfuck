9526709af27e253e4ff68ea5077e058525038905
cosineperson <1774573043@qq.com>
2022-04-18 13:12:07 +0800

    OOP s! It works right...wish so.

--- bfcore.js
+++ bfcore.js
@@ -1,55 +1,175 @@
-const find = require("./connct_finder.js").finder
-exports.run = run
-function run(code,mem=[0],input=[],extra={max:64})
-{
-	//code is a string
-	//place control the place of the pointer~
-	//ctrstr control the stream
-	let rst = []
-	const ctrarr = find(code)
-	let ctrstr=0
-	let place=0
-	while (1)
-	{
-		var codechar = code[ctrstr]
-		switch (codechar.toString())
-		{
-			case "+":
-				mem[place] += 1
-				break
-			case "-":
-				mem[place] -= 1
-				break
-			case ".":
-				rst.push(mem[place])
-				break
-			case ",":
-				mem[place]=input.shift()
-			case ">":
-				if(place < mem.length-1) place++
-				else place=0
-				break
-			case "<":
-				if(place>0) place--
-				else place=mem.length-1
-				break
-			case "[":
-				if (mem[place] == 0)
-				{
-					ctrstr = ctrarr[ctrstr]
-				}
-				break
-			case "]":
-				if (mem[place] != 0)
-				{
-					ctrstr = ctrarr[ctrstr]
-				}
-				break
-			default:
-				break
-		}
-		ctrstr ++
-		if (ctrstr >= code.length) break
-	}
-	return [rst,mem]
+const find = require("./pairs_finder.js").finder;
+
+class Code {
+    constructor(code) {
+        /** @param {String[String]} code */
+        this.code = code;
+        this.controller = 0;
+        let findResult = find(this.code);
+        if (typeof findResult == "number") {
+            // find return NaN, not in pairs
+            this.notAvailble = "not in pairs";
+        } else {
+            // alright
+            this.matchBrancketsList = find(this.code);
+            this.notAvailble = false;
+        }
+    }
+    returnCodeChar() {
+        return this.code[this.controller];
+    }
+    commandNext() {
+        // @info return whether it succeeds
+        if (this.controller >= this.code.length) return false;
+        else {
+            this.controller++;
+            return true;
+        }
+    }
+    brShift() {
+        let ctr = this.controller;
+        this.controller = this.matchBrancketsList[ctr];
+    }
 }
+
+class State {
+    constructor(mem = [0]) {
+        this.pointer = 0;
+        this.memoryplace = mem;
+        /** @param {Number[]} memoryplace */
+    }
+    setMemory(mem) {
+        this.memoryplace = mem;
+    }
+    setPointer(to) {
+        this.pointer = to;
+    }
+    pointerNext() {
+        if (this.pointer == this.memoryplace.length - 1)
+            this.memoryplace.push(0);
+        this.pointer++;
+    }
+    pointerBack() {
+        if (this.pointer == 0) {
+            this.memoryplace.unshift();
+            this.pointer++;
+        }
+        this.pointer--;
+    }
+    memoryFetch() {
+        return this.memoryplace[this.pointer];
+    }
+    memoryWrite(num) {
+        /** @param {Number} num*/
+        this.memoryplace[this.pointer] = num;
+    }
+    memoryChange(method) {
+        let pointer = this.pointer;
+        this.memoryplace[pointer] = method(this.memoryplace[pointer]);
+    }
+    runOnce(code, input) {
+        // @param {Code} code
+        let output = 0;
+        // @param Number output
+        let codechar = code.returnCodeChar();
+        if (code.notAvailble) {
+            return NaN;
+        } else {
+            switch (codechar) {
+                // @todo
+                case "+":
+                    this.memoryChange((x) => x + 1);
+                    break;
+                case "-":
+                    this.memoryChange((x) => x - 1);
+                    break;
+                case ".":
+                    output = this.memoryFetch();
+                    break;
+                case ",":
+                    this.memoryWrite(input.shift());
+                    break;
+                case ">":
+                    this.pointerNext();
+                    break;
+                case "<":
+                    this.pointerBack();
+                    break;
+                case "[":
+                    // do nothing
+                    break;
+                case "]":
+                    if (this.memoryFetch() == 0) {
+                    } else code.brShift();
+                    break;
+            }
+            return code.commandNext() ? output : NaN;
+        }
+    }
+}
+exports.Code = Code;
+exports.State = State;
+exports.run = function (code, input = [0], times = Infinity) {
+    let myState = new State(memory);
+    let myCode = new Code(code);
+    let inputs = input;
+    let res = [];
+    for (let i = 0; i < times; i++) {
+        let returning = myState.runOnce(myCode, inputs);
+        if (isNaN(returning)) break;
+        else res.push(returning);
+    }
+    return res;
+};
+
+// old
+//exports.oldRun = oldRun;
+//function oldRun(code, mem = [0], input = [], extra = { max: 64 }) {
+//    //code is a string
+//    //place control the place of the pointer :D
+//    //ctrstr control the stream
+//    let rst = [];
+//    const ctrarr = find(code);
+//    let ctrstr = 0;
+//    let place = 0;
+//    while (1) {
+//        var codechar = code[ctrstr];
+//        switch (codechar.toString()) {
+//            case "+":
+//                mem[place] += 1;
+//                break;
+//            case "-":
+//                mem[place] -= 1;
+//                break;
+//            case ".":
+//                rst.push(mem[place]);
+//                break;
+//            case ",":
+//                mem[place] = input.shift();
+//            case ">":
+//                if (place < mem.length - 1) place++;
+//                else place = 0;
+//                break;
+//            case "<":
+//                if (place > 0) place--;
+//                else place = mem.length - 1;
+//                break;
+//            case "[":
+//                if (mem[place] == 0) {
+//                    ctrstr = ctrarr[ctrstr];
+//                }
+//                break;
+//            case "]":
+//                if (mem[place] != 0) {
+//                    ctrstr = ctrarr[ctrstr];
+//                }
+//                break;
+//            default:
+//                break;
+//        }
+//        ctrstr++;
+//        if (ctrstr >= code.length) break;
+//    }
+//    // @info return output and memorySpace
+//    return [rst, mem];
+//}
---
+++ pairs_finder.js
@@ -0,0 +1,37 @@
+exports.finder = findPairs;
+function findPairs(array) {
+    /**
+     * @param {Function} finder
+     * @description 输入一个字符串数组，返回一个指示对应括号位置的数组。例如：输入“[a[]]”，返回[ 4, -2, 3, 2, 0 ]
+     * @param {String[String]} array
+     */
+    let scannedfro = []; //所有的前括号位置位置
+    let result = []; //指示括号位置的数组
+    let counter = 0;
+    for (let index = 0; index < array.length; index++) {
+        let item = array[index];
+        if (item == "[") {
+            scannedfro.push(index);
+            result.push(-1);
+            counter++;
+        } else if (item == "]") {
+            result.push(scannedfro.pop(-1));
+            counter--;
+        } else {
+            result.push(-2);
+        }
+    }
+    result.forEach((item, index, arr) => {
+        if (item >= 0) arr[item] = index;
+        //补全剩下一半括号的索引
+    });
+    /**
+  return result.map((item) => {
+    if (typeof item == "string") return parseInt(item, 10);
+    else return item;
+  });
+  */
+    return counter ? NaN : result;
+}
+// test
+// console.log(find('[a[]]'))
